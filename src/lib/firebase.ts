4b032e13231f59da520552f4a6bde2d645bef4a5
Wahi Chaudhary <wahichaudhary31@gmail.com>
2026-04-19 15:42:02 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,10 +3,11 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCRPkwRT0ahq45qVccAskVWPLKIF1U",
+  apiKey: "AIzaSyCRPkwRT0ahqYrnq45qVccAskVWPLKIF1U",
   authDomain: "nexa-library.firebaseapp.com",
+  databaseURL: "https://nexa-library-default-rtdb.firebaseio.com",
   projectId: "nexa-library",
-  storageBucket: "nexa-library.appspot.com",
+  storageBucket: "nexa-library.firebasestorage.app",
   messagingSenderId: "513211376038",
   appId: "1:513211376038:web:fb25e84e89a9512cda2f67",
   measurementId: "G-RRS84WFGEM"
